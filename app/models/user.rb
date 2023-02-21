a76d04fa0c67b994f0a9ead7c4cb4983b85647da
Akvayli06 <benidirifaouzi@gmail.com>
2023-02-21 11:27:44 +0100

    avec device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend Devise::Models
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    after_create :welcome_send
+         
+         after_create :welcome_send
 
     
     has_many :events, class_name: "Event"
